c05aef8ef4507958226075ccef3e499b3b19f859
tsukune <interavook@gmail.com>
2025-10-13 16:46:53 +0900

    エラーあり

--- src/features/transaction/hooks/useTransactionForm.ts
+++ src/features/transaction/hooks/useTransactionForm.ts
@@ -2,11 +2,11 @@ import { useForm } from "react-hook-form";
 import { getCategoriesByType } from "../../../constants/transaction";
 import { transactionSchema } from "../schemas/intex";
 import { valibotResolver } from "@hookform/resolvers/valibot";
-import type { FormData } from "../types";
+import type { FormData, FormInput } from "../types";
 
 export const useTransactionForm = () => {
   const { register, handleSubmit, watch, setValue, reset, formState } =
-    useForm<FormData>({
+    useForm<FormInput>({
       defaultValues: {
         type: "expense",
         amount: "",
@@ -14,7 +14,8 @@ export const useTransactionForm = () => {
         date: "",
         memo: "",
       },
-      resolver: valibotResolver(transactionSchema),
+      //resolver: valibotResolver(transactionSchema),
+      resolver: valibotResolver<FormData>(transactionSchema),
       mode: "onSubmit",
       reValidateMode: "onChange",
     });
@@ -25,12 +26,11 @@ export const useTransactionForm = () => {
 
   return {
     register,
-    handleSubmit,
     watch,
     setValue,
     reset,
     errors: formState.errors,
     categories: getCategoriesByType(watch("type")),
-    onSubmit,
+    onSubmit: handleSubmit(onSubmit),
   };
 };
--- src/features/transaction/types/index.ts
+++ src/features/transaction/types/index.ts
@@ -1,6 +1,14 @@
-export type FormData = {
+export type FormInput = {
   type: "income" | "expense";
   amount: string;
+  category: string;
+  date: string;
+  memo: string;
+};
+
+export type FormData = {
+  type: "income" | "expense";
+  amount: number; // 変換後は数値！
   category: string;
   date: string;
   memo: string;
